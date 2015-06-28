c97c73737856f504dbbfa520f15a8a6063d6a55e
Sharad Vikram <sharad.vikram@gmail.com>
2015-06-27 22:56:11 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "deepx",
+    version = "0.0.0",
+    author = "Zachary Chase Lipton, Sharad Vikram",
+    author_email = "sharad.vikram@gmail.com",
+    license = "MIT",
+    keywords = "theano",
+    packages=find_packages(include=[
+        'deepx'
+    ]),
+    classifiers=[
+],
+)
