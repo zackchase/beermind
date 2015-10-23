335478be4a4c538e29e994f1346227d44f148944
Sharad Vikram <sharad.vikram@gmail.com>
2015-10-23 02:07:49 -0700

    minor fixes to helper

--- deepx/nn/helper.py
+++ deepx/nn/helper.py
@@ -1,17 +1,16 @@
 import numpy as np
 import theano
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
 
 srng = RandomStreams()
-
 
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
 
-
 def scale(X, max_norm):
     curr_norm = T.sum(T.abs_(X))
-    return ifelse(T.lt(curr_norm, max_norm), X, max_norm * (X/curr_norm))
+    return T.ifelse(T.lt(curr_norm, max_norm), X, max_norm * (X/curr_norm))
 
 def dropout(X, p=0.):
     if p > 0:
@@ -19,3 +18,4 @@ def dropout(X, p=0.):
         X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
         X /= retain_prob
         return X
+    return X
