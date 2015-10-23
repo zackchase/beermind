76abd2a230b5d00b05cb8719660184a5e98d1ab6
Zack <zachary.chase@gmail.com>
2015-10-23 01:54:10 -0700

    added code for dropout

--- deepx/nn/helper.py
+++ deepx/nn/helper.py
@@ -8,3 +8,10 @@ def floatX(X):
 def scale(X, max_norm):
     curr_norm = T.sum(T.abs_(X))
     return ifelse(T.lt(curr_norm, max_norm), X, max_norm * (X/curr_norm))
+
+def dropout(X, p=0.):
+    if p > 0:
+        retain_prob = 1 - p
+        X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
+        X /= retain_prob
+        return X
