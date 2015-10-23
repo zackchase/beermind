58c7c05ecf4191fc7bdaa44ce6e783da258499a9
Zack <zachary.chase@gmail.com>
2015-10-22 23:01:49 -0700

    adding helper code for gradient scaling

--- deepx/nn/helper.py
+++ deepx/nn/helper.py
@@ -3,3 +3,8 @@ import theano
 
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
+
+
+def scale(X, max_norm):
+    curr_norm = T.sum(T.abs_(X))
+    return ifelse(T.lt(curr_norm, max_norm), X, max_norm * (X/curr_norm))
